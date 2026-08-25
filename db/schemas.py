f1dc1356eff6f72e2f129c46b0071207f6028fa3
Chan Park <chan95821@naver.com>
2026-08-25 19:34:52 +0900

    feat: added comment document schema, working on repository cruds

--- db/schemas.py
+++ db/schemas.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timezone, timedelta
+from zoneinfo import ZoneInfo
+
 from bson import ObjectId
 from enum import Enum
 from pydantic import BaseModel, ConfigDict, Field
@@ -6,23 +9,36 @@ class PresentationStatus(str, Enum): # 정상 업로드 이후여야
     WAITING = "waiting"
     CONVERTING = "converting"
     CONVERTED = "converted"
-
+    FAILED = "failed"
 
 class UserDB(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
-    id: ObjectId = Field(alias="_id")
+    id: ObjectId = Field(default_factory=ObjectId, alias="_id")
     user_name: str
     user_id: str
-    presentations_oid: list[ObjectId]
+    presentations_oid: list[ObjectId] = Field(default_factory=list)
+    password: bytes
 
-class PresentationDB(BaseModel):
+class PresentationDB(BaseModel): 
     model_config = ConfigDict(arbitrary_types_allowed=True)
     id: ObjectId = Field(alias="_id")
     
-    user_oid = ObjectId
+    user_oid: ObjectId
     slides_oid: list[ObjectId]
     status : PresentationStatus = PresentationStatus.WAITING
+    created_at: datetime = Field(
+    default_factory=lambda: datetime.now(ZoneInfo("Asia/Seoul"))
+)
 
 class SlideDB(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
     img_src: str
-    replies: list[str]
+    replies: list[ObjectId]
+
+class CommentDB(BaseModel):
+    reply: str
+    user_oid : ObjectId
+    created_at: datetime = Field(
+    default_factory=lambda: datetime.now(ZoneInfo("Asia/Seoul"))
+)
+
